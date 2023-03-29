21325e564ba022d9d4b9ce801110e88bc41e398e
David Dume <dumedavid11@gmail.com>
2023-03-29 15:49:04 +0200

    componete header completato

---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<script>
+    export default {
+        name: 'Header',
+        props: {
+            title: String
+        }
+    }
+</script>
+
+<template>
+
+    <div class="header">
+        <h1>{{ title }}</h1>
+    </div>
+
+</template>
+
+<style lang="scss">
+    .header {
+        padding: 20px 0;
+        text-align: center;
+    }
+</style>
